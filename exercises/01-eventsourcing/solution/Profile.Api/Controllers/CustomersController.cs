c28535d83c6e2e860734ef016a4cd05cdba9e09b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-08-26 07:38:59 +0200

    Add logic to REST endpoints

---
+++ exercises/01-eventsourcing/solution/Profile.Api/Controllers/CustomersController.cs
@@ -0,0 +1,56 @@
+﻿using Marten;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Profile.Api.Domain;
+using Profile.Api.Forms;
+
+namespace Profile.Api.Controllers;
+
+[ApiController]
+[Route("/customers")]
+public class CustomersController : ControllerBase
+{
+    private readonly IDocumentSession _documentSession;
+
+    public CustomersController(IDocumentSession documentSession)
+    {
+        _documentSession = documentSession;
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> RegisterCustomer(RegisterCustomerForm form)
+    {
+        var customerId = Guid.NewGuid();
+
+        var customer = new Customer(
+            customerId,
+            form.FirstName,
+            form.LastName,
+            form.InvoiceAddress,
+            form.ShippingAddress,
+            form.EmailAddress);
+
+        _documentSession.Events.StartStream<Customer>(customerId, customer.PendingDomainEvents);
+        await _documentSession.SaveChangesAsync();
+
+        return Accepted();
+    }
+
+    [HttpPost("{customerId:guid}/unsubscribe")]
+    public async Task<IActionResult> CancelSubscription(Guid customerId)
+    {
+        var customer = await _documentSession.Events.AggregateStreamAsync<Customer>(customerId);
+
+        if (customer == null)
+        {
+            return NotFound();
+        }
+
+        customer.Unsubscribe();
+
+        _documentSession.Events.Append(customerId, customer.PendingDomainEvents);
+        await _documentSession.SaveChangesAsync();
+
+        return Accepted();
+    }
+}
--- exercises/01-eventsourcing/solution/Profile.Api/Program.cs
+++ exercises/01-eventsourcing/solution/Profile.Api/Program.cs
@@ -1,6 +1,18 @@
+using Marten;
+using Weasel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMarten(marten =>
+{
+    marten.Connection(builder.Configuration.GetConnectionString("DefaultDatabase"));
+    marten.AutoCreateSchemaObjects = AutoCreate.CreateOnly;
+});
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
